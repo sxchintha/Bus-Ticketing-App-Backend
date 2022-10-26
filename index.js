b18177a8a3a095f43c81ddb49e293d598e527029
Yasith Chandula <76646376+yasithchandula@users.noreply.github.com>
2022-10-26 15:25:41 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8070;
+const URL = process.env.MONGODB_URL;
+
+app.use(cors);
+
+
+
+
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("Mongodb connection success!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`);
+});
